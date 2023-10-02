5651cbc98dc1dd979b84a4d339754c220f2c041f
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2023-10-02 14:15:56 -0400

    v0.01c

--- gameProgramming/computer_science_exercises/02_collections/collections.py
+++ gameProgramming/computer_science_exercises/02_collections/collections.py
@@ -10,6 +10,17 @@ testscores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1.74, 1.99, 0.99,4.25]
 
 #printing contents of an list
-print(breakfastfoods)
-print(testscores)
-print(classGPA)
+#print(breakfastfoods)
+#print(testscores)
+#print(classGPA)
+
+#access specific elements ----- remember first elements is 0
+#print(breakfastfoods[0])
+#print(testscores[0])
+#print(classGPA[0])
+
+# accessing the last element
+#print(breakfastfoods[-1])
+#print(classGPA[-1])
+#print(testscores[-1])
+

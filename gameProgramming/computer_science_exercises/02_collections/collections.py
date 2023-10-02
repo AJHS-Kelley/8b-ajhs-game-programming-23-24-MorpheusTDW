89a2a62befdd47cb47cd1850d1bb134b2616702e
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2023-10-02 14:06:42 -0400

    v0.1a
    versions

--- gameProgramming/computer_science_exercises/02_collections/collections.py
+++ gameProgramming/computer_science_exercises/02_collections/collections.py
@@ -1,1 +1,10 @@
 # collection examples , phillip henry, v0.0
+# list--- ordered, changeable, allows duplicate values
+#
+breakfastfoods = ["waffles","pancakes" , "cereal", "milk"]
+# each iteam on the list is known as an element
+#the postion in the list for each is the index
+# the element "bacon" is at index 0.
+# python only: index -1 it is the last item on the list
+testscores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99,4.25]

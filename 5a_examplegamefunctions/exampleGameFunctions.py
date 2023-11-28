8c77283b9ff2258c32488a1204bc8ad0c1205e16
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2023-11-28 13:59:01 -0500

    added the date selection

--- 5a_examplegamefunctions/exampleGameFunctions.py
+++ 5a_examplegamefunctions/exampleGameFunctions.py
@@ -1,8 +1,14 @@
 #example game functions project phillip henry v1.0
 import random
-def functionOne():
-    pass
+print("Welcome to the muticlash!")
 
+print("""
+    *~~~~~~~~~~~~~~~~~~~~~~~~~*
+     |     welcome to          |
+     |     multiclash!!!       |
+     |                         |
+     |                         |
+     |~~~~~~~~~~~~~~~~~~~~~~~~~* """)
 
 def playerselect(row, playernumber, charcterpick, column ):
     if row > 5 and playernumber >= 1 and charcterpick == 'kimberly':
@@ -20,13 +26,22 @@ def choosefighter(row):
         playerselect = 'anyone from row 5'
         fightersfromrow5 = ('kimberly, jack, reaper, chaos, scorpion, zero, kasmo')
         fighterlist = ['kimberly']
-
-print("Welcome to the Fighting Game!")   
+   # select map
+   
 maps = ["City", "Forest", "Arena"]
 print("Select a Map:")
 for i, map_option in (maps, 1):
     print(f"{i}. {map_option}")
     map_choice = (input("Enter the number of your chosen map: "))
     selected_map = maps[map_choice - 1]
-    
+
+    # Select Day
+    days = ["Monday", "Wednesday", "Friday"]
+    print("Select a Day:")
+    for i, day_option in (days, 1):
+        print(f"{i}. {day_option}")
+    day_choice = int(input("Enter the number of your chosen day: "))
+    selected_day = days[day_choice - 1]
+
+
    

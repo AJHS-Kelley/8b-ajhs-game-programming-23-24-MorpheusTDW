0108841bf166303ff348dee8483d9a0ebffb5383
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2023-11-30 14:49:01 -0500

    examplefunction.py
    game

--- 5a_examplegamefunctions/exampleGameFunctions.py
+++ 5a_examplegamefunctions/exampleGameFunctions.py
@@ -42,15 +42,31 @@ else:
       map_choice = 3
       selected_map = maps[map_choice - 1]
       print(selected_map)
+day_choice = int(input("Enter the number of your chosen day 1 for monday 2 for wendsday and 3 for friday"))
+if day_choice <= 3 and day_choice >= 1:
+      print(day_choice)
+      print("goodjob for chossing that lets GO!")
+      name = input("Enter your name: ")
+print(f"Welcome, {name}!")
+print("Choose your character:")
+print("1. kimberly")
+print("2. reaper")
+print("3. goku")
+character_choice = input("Enter the number corresponding to your choice: ")
+print(f"ok so your fighting at the {selected_map} on the day of {day_choice} with the character{character_choice} ok lets fight {name}")
+
+print("""
+    *~~~~~~~~~~~~~~~~~~~~~~~~~*
+     |     FIGHT!!!         2
+     |
+     | 0              0         |
+     |/|\            /|\        |
+     |/_\____________/_\_______ |
+     |~~~~~~~~~~~~~~~~~~~~~~~~~* """)      
 
 
-    # Select Day
-#days = ["Monday", "Wednesday", "Friday"]
-#print("Select a Day:")
-#for i, day_option in (days, 1):
-#        print(f"{i}. {day_option}")
-#day_choice = int(input("Enter the number of your chosen day: "))
-#selected_day = days[day_choice - 1]
+    
+
 
 
    

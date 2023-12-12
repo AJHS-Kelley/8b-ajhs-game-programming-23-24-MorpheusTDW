5ce54f27318e0fcba053aad173483f8dd448bc66
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2023-12-12 14:11:51 -0500

    stuff

--- 5a_examplegamefunctions/06_oop/oop.py
+++ 5a_examplegamefunctions/06_oop/oop.py
@@ -6,6 +6,21 @@ class person: # use pascal for classname
         self.weight = weight 
     
     def __str__(self):
-        return f"name: {self.name}\n this person is {self.name}\nthis person is {self.age} he weights {self.weight}
+        return f"name: {self.name}\nthis person is {self.age} years old\nthey weigh {self.weight} pounds. \n"
 
-print(person1) = person("phillip henry",2,150)
+person1 = person ("phillip",17,250.5)
+person2 = person("jordan",13,150.5)
+print(person)
+print(person2)
+
+if person.age > person2.age:
+    print(f"person.name) haha shes older than you (person2.name),")
+elif person.weight == person2.weight:
+    print("each person weighs the time. \n")
+else:
+    print(f"(person2.name)weighs more than (person.name).")
+
+
+
+        
+        

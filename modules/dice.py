fa7155dee3f275ce8e4d622846a13d893859f582
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2023-12-14 13:57:39 -0500

    double

--- modules/dice.py
+++ modules/dice.py
@@ -17,3 +17,17 @@ def rolldice(numdice,sizedice):
         numrolled += 1
         sum = 0
         return sum
+    def isDoubles(roll1,roll2):
+        if roll1 == roll2:
+            isDoubles = True
+        else:
+            isDoubles = False
+            return isDoubles
+        
+        roll1 = dice.roll(1,6)
+        roll2 = dice.display(1,6)
+
+        if dice.isDoubles(roll, roll2):
+            print("you rolled a double, go again!\n")
+        else:
+            print("it was not a double")

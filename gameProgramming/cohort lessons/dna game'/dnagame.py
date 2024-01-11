3e03fd0ac081b2dc61f10f6ee1381876f2dbcc6c
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2024-01-11 14:47:29 -0500

    dnagame.py
    gamedna

--- gameProgramming/cohort lessons/dna game'/dnagame.py
+++ gameProgramming/cohort lessons/dna game'/dnagame.py
@@ -14,4 +14,16 @@ def genDNA() -> str:
     baserequested = int(input("please enter a positive interger number of bases to generate.\n"))
 
     while basegenerated < baserequested:
-    
+
+        
+
+def genRNA(dnasequence: str) -> tuple:
+    print(f"the DNA sequence is {dnasequence}.\n")
+    print("you will now genarate the dna sequence the would match\n")
+    print("please remember, in the RNA sequence u pairs with a from the DNAsequence.\n")
+    rnastart = time.time()
+    rnasequence = input("please enter the matching rna sequence . leave no space! then press enter\n").upper()
+    rnastop = time.time()
+    rnatime = rnastop - rnastart
+    return (rnasequence, rnatime)
+

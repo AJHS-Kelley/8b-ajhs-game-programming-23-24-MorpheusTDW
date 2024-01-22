2bf155d4737cb0bc8c510f77b4cebd8e0754fcc8
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2024-01-22 13:57:53 -0500

    NUMBERSLIDER.PY

--- 07_numberslider/numberslider.py
+++ 07_numberslider/numberslider.py
@@ -1,3 +1,13 @@
 #number slider, by phillip henry, based on a project by al swelgart, v0.0
 import sys, random, pygame
 #sys module provides access to system resources (i.e. operating system commands)
+from pygame.locals import *
+
+BOARDWIDTH = 4
+BOARDHEIGHT = 4
+TILESIZE = 80 
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+FPS = 30
+BLANK = None
+

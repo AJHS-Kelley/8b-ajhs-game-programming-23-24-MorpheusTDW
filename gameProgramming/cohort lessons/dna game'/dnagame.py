d50469a9aaca448250384bbddd6adedd1145fca5
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2024-02-01 13:46:34 -0500

    dnagame

--- gameProgramming/cohort lessons/dna game'/dnagame.py
+++ gameProgramming/cohort lessons/dna game'/dnagame.py
@@ -1,52 +1,25 @@
-#dna rep game , phil v0.0
-import time,datetime
+# Import Entire Modules -- Get the whole tool box
+import time, datetime
 
-import random
-import
+# Import Specific Methods -- Get the specific tool
+from random import choice
 
-dnabases = ["A", "T", "G" "C"]
+# Store the DNA Bases
+dnaBases = ["A", "T", "G", "C"]
 
-def gameintro() -> none:
+# Game Functions
+def gameIntro() -> None:
     pass
 
 def genDNA() -> str:
-    basegenerated = 0
-    baserequested = int(input("please enter a positive interger number of bases to generate.\n"))
+    basesGenerated = 0
+    basesRequested = int(input("How many bases do you want, Please enter a possitive number\n"))
+    dnaSequence = ""
 
-    while basegenerated < baserequested:
-
-        
-
-def doTranscriptiongenRNA(dnasequence: str) -> tuple:
-    print(f"the DNA sequence is {dnasequence}.\n")
-    print("you will now genarate the dna sequence the would match\n")
-    print("please remember, in the RNA sequence u pairs with a from the DNAsequence.\n")
-    rnastart = time.time()
-    rnasequence = input("please enter the matching rna sequence . leave no space! then press enter\n").upper()
-    rnastop = time.time()
-    rnatime = rnastop - rnastart
-    return (rnasequence, rnatime)
-
-def verifysequence(dnasequence: str, rnasequence: str) -> bool:
-    ismatch = False
-    if len(dnasequence) != len(rnasequence):
-         dnabases, rnabase in zip(dnasequence, rnasequence):
-    x    if dnabases == "A" and rnabase == "U":
-                ismatch = True
-               elif dnabases C
-                elif dnabases G
-                elif dnabases T
-
-
-    return ismatch
-
-
-
-rna = doTranscriptiongenRNA(dna)
-print(rna)
-
-if len(dnasequence: str, rnasequence: str):
-    print("the sequence are diffrent length and cannot match.\n")
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+    return dnaSequence
 
     
 

1e8110b7301c52e68dd74140d7de62a7d20b0fca
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2024-02-01 13:50:16 -0500

    dnagame.py

--- gameProgramming/cohort lessons/dna game'/dnagame.py
+++ gameProgramming/cohort lessons/dna game'/dnagame.py
@@ -21,6 +21,17 @@ def genDNA() -> str:
         basesGenerated += 1
     return dnaSequence
 
+def doTranscription(dnaSequence: str, basesRequested: str) -> tuple:
+    print(f"The DNA Sequence is {dnaSequence}.\n")
+    print("You will now generate the RNA sequence that would match\n")
+    print("Please remember, in the RNA sequence U pairs with A from the DNA sequence")
+    rnaStart = time.time() # time.time() returns the number of seconds since 00:00:00 UTC Jan, 01, 1970
+    rnaStop = time.time()
+    rnaTime = rnaStop - rnaStart
+    rnaSequence = input("Please enter the matching RNA sequence.  Leave no spaces! Then press enter.\n").upper().split()
+    return (rnaSequence, rnaTime)
+
+
     
 
     

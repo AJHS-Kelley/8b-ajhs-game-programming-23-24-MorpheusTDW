f8c1d57714e005dd7e08eabdfcc8f4f7bc3d26a5
MorpheusTDW <142345377+MorpheusTDW@users.noreply.github.com>
2024-04-11 14:18:33 -0400

    ultimate

--- UltimatePygameIntro-main/UltimatePygameIntro-main/runner_class only.py
+++ UltimatePygameIntro-main/UltimatePygameIntro-main/runner_class only.py
@@ -7,17 +7,17 @@ from random import randint, choice
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
-		player_walk_1 = pygame.image.load('UltimatePygameIntro-main/graphics/player/player_walk_1.png').convert_alpha()
-		player_walk_2 = pygame.image.load('UltimatePygameIntro-main/graphics/player/player_walk_2.png').convert_alpha()
+		player_walk_1 = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/player/player_walk_1.png').convert_alpha()
+		player_walk_2 = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/player/player_walk_2.png').convert_alpha()
 		self.player_walk = [player_walk_1,player_walk_2]
 		self.player_index = 0
-		self.player_jump = pygame.image.load('UltimatePygameIntro-main/graphics/player/jump.png').convert_alpha()
+		self.player_jump = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/player/jump.png').convert_alpha()
 
 		self.image = self.player_walk[self.player_index]
 		self.rect = self.image.get_rect(midbottom = (80,300))
 		self.gravity = 0
 
-		self.jump_sound = pygame.mixer.Sound('UltimatePygameIntro-main/audio/jump.mp3')
+		self.jump_sound = pygame.mixer.Sound('UltimatePygameIntro-main/UltimatePygameIntro-main/audio/jump.mp3')
 		self.jump_sound.set_volume(0.5)
 
 	def player_input(self):
@@ -50,8 +50,8 @@ class Obstacle(pygame.sprite.Sprite):
 		super().__init__()
 		
 		if type == 'fly':
-			fly_1 = pygame.image.load('UltimatePygameIntro-main/graphics/fly/fly1.png').convert_alpha()
-			fly_2 = pygame.image.load('UltimatePygameIntro-main/graphics/fly/fly2.png').convert_alpha()
+			fly_1 = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/fly/fly1.png').convert_alpha()
+			fly_2 = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/fly/fly2.png').convert_alpha()
 			self.frames = [fly_1,fly_2]
 			y_pos = 210
 		else:
@@ -96,11 +96,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('UltimatePygameIntro-main/font/Pixeltype.ttf', 50)
+test_font = pygame.font.Font('UltimatePygameIntro-main/UltimatePygameIntro-main/font/Pixeltype.ttf', 50)
 game_active = False
 start_time = 0
 score = 0
-bg_music = pygame.mixer.Sound('UltimatePygameIntro-main/audio/music.wav')
+bg_music = pygame.mixer.Sound('UltimatePygameIntro-main/UltimatePygameIntro-main/audio/music.wav')
 bg_music.play(loops = -1)
 
 #Groups
@@ -109,8 +109,8 @@ player.add(Player())
 
 obstacle_group = pygame.sprite.Group()
 
-sky_surface = pygame.image.load('UltimatePygameIntro-main/graphics/Sky.png').convert()
-ground_surface = pygame.image.load('UltimatePygameIntro-main/graphics/ground.png').convert()
+sky_surface = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/ground.png').convert()
 
 # Intro screen
 player_stand = pygame.image.load('UltimatePygameIntro-main/graphics/player/player_stand.png').convert_alpha()
